9d8b91565c53eabdb341d7b7d7dde6dda9b992c2
davyd.nykytiuk <davyd.nykytiuk@rubyplay.com>
2025-08-04 19:02:19 +0300

    comment logging

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    # logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())
     except (KeyboardInterrupt):
